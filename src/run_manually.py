96a36e8f3c9d2ecff94709569f048b0a481a29bc
Julian <julian.foerster@mavt.ethz.ch>
2019-12-15 20:40:35 +0100

    WIP

--- src/run_manually.py
+++ src/run_manually.py
@@ -20,6 +20,8 @@ def drawer_example(sk_grasp, robot, scene, world):
     # Run move skill
     move_to_object("cupboard", scene, robot._model.uid)
 
+    # robot.get_wrist_force()
+
     # Grasp the cupboard handle
     res = sk_grasp.grasp_object("cupboard", scene.objects["cupboard"].grasp_links[3])
     if not res:
@@ -27,8 +29,10 @@ def drawer_example(sk_grasp, robot, scene, world):
         return
 
     # Drive back
-    robot.update_velocity([0.0, -0.1, 0.0], 0.0)
-    world.step_seconds(4)
+    robot.update_velocity([-0.1, 0.0, 0.0], 0.0)
+    world.step_seconds(2)
+    # robot.get_wrist_force()
+    world.step_seconds(2)
     robot.stop_driving()
 
     # Release
@@ -39,8 +43,36 @@ def cube_example(sk_grasp, robot, scene, sk_place):
     # Run move skill
     move_to_object("cube1", scene, robot._model.uid)
 
+    print("empty hand:")
+    robot._world.step_seconds(1.0)
+    print(robot.get_wrist_force())
+
     # Grasp the cube
     sk_grasp.grasp_object("cube1")
+
+    robot.to_start()
+    print("after homing:")
+    robot._world.step_seconds(1.0)
+    print(robot.get_wrist_force())
+
+    from math import pi as m_pi
+
+    robot.transition_cmd_to(np.array([0, -m_pi/4.0, 0, -3.0*m_pi/4.0, 0, m_pi/4.0, m_pi/4.0]))
+    print("after tilting back:")
+    robot._world.step_seconds(1.0)
+    print(robot.get_wrist_force())
+
+    robot.transition_cmd_to(np.array([0, -m_pi/4.0, 0, -3.0*m_pi/4.0, 0, m_pi, m_pi/4.0]))
+    print("after tilting forward:")
+    robot._world.step_seconds(1.0)
+    print(robot.get_wrist_force())
+
+    robot.transition_cmd_to(np.array([0, -m_pi/4.0, 0, -3.0*m_pi/4.0, m_pi/2.0, m_pi, m_pi/4.0]))
+    print("after rolling:")
+    robot._world.step_seconds(1.0)
+    print(robot.get_wrist_force())
+
+    return
 
     # Place cube somewhere else
     sk_place.place_object(scene.objects["cube1"].init_pos + np.array([0.0, 0.2, 0.0]))
@@ -66,8 +98,8 @@ def main():
 
     # Create world
     world = World(gui_=True, sleep_=True, load_objects=not restore_existing_objects)
-    # scene = ScenePlanning1(world, restored_objects=objects)
-    scene = SceneMoveSkill(world, restored_objects=objects)
+    scene = ScenePlanning1(world, restored_objects=objects)
+    # scene = SceneMoveSkill(world, restored_objects=objects)
 
     # Spawn robot
     robot = RobotArm(world, robot_mdl)
@@ -84,9 +116,9 @@ def main():
 
     # drawer_example(sk_grasp, robot, scene, world)
 
-    # cube_example(sk_grasp, robot, scene, sk_place)
+    cube_example(sk_grasp, robot, scene, sk_place)
 
-    drive_example(robot, world)
+    # drive_example(robot, world)
 
     # -----------------------------------
 
