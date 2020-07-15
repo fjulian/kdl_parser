426a8f4b4555669075a105bb6f7f3d83913687cb
Julian Förster <julian.foerster@mavt.ethz.ch>
2020-07-15 18:59:32 +0200

    adapted deps

---
+++ install_requirements.sh
@@ -0,0 +1,6 @@
+sudo apt-get install ros-$ROS_DISTRO-trac-ik \
+    ros-$ROS_DISTRO-franka-description \
+    ros-$ROS_DISTRO-ridgeback-description \
+    ros-$ROS_DISTRO-joint-state-publisher-gui \
+    python3-virtualenv \
+    python3-catkin-tools
