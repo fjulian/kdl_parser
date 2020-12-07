8ee0190e913dd908d92eedc60fe09a0e587639a0
Julian Förster <julian.foerster@mavt.ethz.ch>
2020-12-07 18:45:35 +0100

    GUI solution for running and pausing simulation

--- highlevel_planning/scripts/gui.py
+++ highlevel_planning/scripts/gui.py
@@ -1,46 +1,65 @@
 # from tkinter import *
 import tkinter as tk
 from tkinter import ttk
+import rospy
+from std_srvs.srv import SetBool, SetBoolRequest, Trigger, TriggerRequest
 
 
 class Application:
     def __init__(self):
-        self.root = None
+        self.root = tk.Tk()
         self.feet = None
         self.meters = None
+        self.running = tk.BooleanVar()
+
+        rospy.loginfo("Waiting for server ...")
+        get_srv = rospy.ServiceProxy("sim_status", Trigger)
+        get_srv.wait_for_service()
+        res = get_srv(TriggerRequest())
+        self.running.set(res.success)
+        rospy.loginfo("Got sim status, setting up window")
+
+        self.set_srv = rospy.ServiceProxy("sim_switch", SetBool)
+
         self.setup_window()
 
     def setup_window(self):
-        self.root = tk.Tk()
-        self.root.title("Feet to Meters")
+        self.root.title("Simulation Control")
 
         mainframe = ttk.Frame(self.root, padding="3 3 12 12")
         mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
 
-        self.feet = tk.StringVar()
-        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-        feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+        # self.feet = tk.StringVar()
+        # feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
+        # feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+        #
+        # self.meters = tk.StringVar()
+        # ttk.Label(mainframe, textvariable=self.meters).grid(
+        #     column=2, row=2, sticky=(tk.W, tk.E)
+        # )
 
-        self.meters = tk.StringVar()
-        ttk.Label(mainframe, textvariable=self.meters).grid(
-            column=2, row=2, sticky=(tk.W, tk.E)
+        ttk.Label(mainframe, textvariable=self.running).grid(
+            column=1, row=2, sticky=(tk.W,)
         )
 
-        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(
-            column=3, row=3, sticky=tk.W
+        ttk.Button(mainframe, text="Run", command=self._run_sim).grid(
+            column=1, row=1, sticky=tk.W
+        )
+        ttk.Button(mainframe, text="Stop", command=self._stop_sim).grid(
+            column=2, row=1, sticky=tk.W
         )
 
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W)
+        # ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W)
+        # ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E)
+        # ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W)
 
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
-        feet_entry.focus()
-        self.root.bind("<Return>", self.calculate)
+        # feet_entry.focus()
+        self.root.bind("<Return>", self._run_sim)
 
     def calculate(self, *args):
         try:
@@ -49,10 +68,27 @@ class Application:
         except ValueError:
             pass
 
+    def _run_sim(self, *args):
+        print("Sent run command")
+        res = self.set_srv(SetBoolRequest(data=True))
+        if not res.success:
+            raise RuntimeError
+        self.running.set(True)
+        self.root.bind("<Return>", self._stop_sim)
+
+    def _stop_sim(self, *args):
+        print("Sent stop command")
+        res = self.set_srv(SetBoolRequest(data=False))
+        if res.success:
+            raise RuntimeError
+        self.running.set(False)
+        self.root.bind("<Return>", self._run_sim)
+
     def mainloop(self):
         self.root.mainloop()
 
 
 if __name__ == "__main__":
+    rospy.init_node("hlp_gui")
     app = Application()
     app.mainloop()
--- highlevel_planning/scripts/run_predicate_learning.py
+++ highlevel_planning/scripts/run_predicate_learning.py
@@ -1,5 +1,4 @@
 from highlevel_planning.sim.scene_planning_1 import ScenePlanning1
-from highlevel_planning.sim.scene_move_skill import SceneMoveSkill
 from highlevel_planning.skills.navigate import SkillNavigate
 from highlevel_planning.skills.grasping import SkillGrasping
 from highlevel_planning.skills.placing import SkillPlacing
@@ -12,187 +11,56 @@ import pybullet as p
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import os
+import tkinter as tk
+import rospy
+from std_srvs.srv import SetBool, SetBoolResponse, Trigger, TriggerResponse
 
 
 BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-def drawer_example(sk_grasp, sk_nav, robot, world):
-    # Run move skill
-    sk_nav.move_to_object("cupboard")
+class SimServer:
+    def __init__(self):
+        self.running = False
 
-    print(robot.get_wrist_force_torque())
+        # Load existing simulation data if desired
+        savedir = os.path.join(BASEDIR, "data", "sim")
+        objects, robot_mdl = run_util.restore_pybullet_sim(savedir, args)
 
-    # Grasp the cupboard handle
-    res = sk_grasp.grasp_object("cupboard", link_idx=3)
-    if not res:
-        print("Grasping the handle failed.")
-        return
+        # Load config file
+        cfg = ConfigYaml(os.path.join(BASEDIR, "config", "main.yaml"))
 
-    # Drive back
-    robot.update_velocity([-0.1, 0.0, 0.0], 0.0)
-    world.step_seconds(2)
-    print(robot.get_wrist_force_torque())
-    world.step_seconds(2)
-    robot.stop_driving()
+        # Create world
+        scene, self.world = run_util.setup_pybullet_world(
+            ScenePlanning1, BASEDIR, savedir, objects, args
+        )
 
-    # Release
-    sk_grasp.release_object()
-    robot.to_start()
+        self.run_srv = rospy.Service("sim_switch", SetBool, self._set_run_callback)
+        self.status_srv = rospy.Service("sim_status", Trigger, self._get_run_callback)
 
+    def _set_run_callback(self, req):
+        self.running = req.data
+        res = SetBoolResponse(success=self.running)
+        return res
 
-def drawer_example_auto(sk_grasp, sk_nav, sk_move, robot, scene):
-    # Run move skill
-    sk_nav.move_to_object("cupboard", nav_min_dist=1.0)
+    def _get_run_callback(self, req):
+        res = TriggerResponse(success=self.running)
+        return res
 
-    # Grasp the cupboard handle
-    res = sk_grasp.grasp_object("cupboard", link_idx=3)
-    if not res:
-        print("Grasping the handle failed.")
-        return
-
-    # Run the move skill
-    sk_move.move_object(0.3, np.array([-1.0, 0.5, 0.0]))
-    # sk_move.move_object(0.3, np.array([-0.5, 0.0, -0.5]))     # GT
-
-    # Release
-    sk_grasp.release_object()
-    robot.to_start()
-
-
-def grasp_example(
-    sk_grasp, sk_nav, robot, scene, sk_place, object_name="cube1", link_idx=0
-):
-    # Run nav skill
-    sk_nav.move_to_object(object_name)
-
-    # print("empty hand:")
-    # robot._world.step_seconds(1.0)
-    # print(robot.get_wrist_force_torque())
-
-    # Grasp the object
-    sk_grasp.grasp_object(object_name, link_idx=link_idx, grasp_id=0)
-
-    robot.to_start()
-    print("after homing:")
-    robot._world.step_seconds(1.0)
-    print(robot.get_wrist_force_torque())
-
-    # from math import pi as m_pi
-
-    # robot.transition_cmd_to(
-    #     np.array([0, -m_pi / 4.0, 0, -3.0 * m_pi / 4.0, 0, m_pi / 4.0, m_pi / 4.0])
-    # )
-    # print("after tilting back:")
-    # robot._world.step_seconds(1.0)
-    # print(robot.get_wrist_force())
-
-    # robot.transition_cmd_to(
-    #     np.array([0, -m_pi / 4.0, 0, -3.0 * m_pi / 4.0, 0, m_pi, m_pi / 4.0])
-    # )
-    # print("after tilting forward:")
-    # robot._world.step_seconds(1.0)
-    # print(robot.get_wrist_force())
-
-    # robot.transition_cmd_to(
-    #     np.array([0, -m_pi / 4.0, 0, -3.0 * m_pi / 4.0, m_pi / 2.0, m_pi, m_pi / 4.0])
-    # )
-    # print("after rolling:")
-    # robot._world.step_seconds(1.0)
-    # print(robot.get_wrist_force())
-
-    # Place cube somewhere else
-    new_pos = scene.objects[object_name].init_pos + np.array([0.2, 0.4, 0.05])
-    sk_nav.move_to_pos(new_pos)
-    sk_place.place_object(new_pos)
-
-    robot.to_start()
-    robot._world.step_seconds(1.0)
-
-
-def navigate_with_cube(sk_nav, sk_grasp):
-    # Run move skill
-    sk_nav.move_to_object("cube1")
-
-    # Grasp the cube
-    sk_grasp.grasp_object("cube1")
-
-    sk_nav.move_to_pos(np.array([0.0, 0.0, 0.0]), nav_min_dist=0.3)
-
-
-def navigation_example(sk_nav, world):
-    sk_nav.move_to_object("cube1")
-    world.step_seconds(1.5)
-    sk_nav.move_to_object("lid1")
-    world.step_seconds(1.5)
-    sk_nav.move_to_object("cupboard")
-    world.step_seconds(1.5)
-    sk_nav.move_to_object("container2")
-    world.step_seconds(1.5)
-    sk_nav.move_to_object("cube1")
-    world.step_seconds(1.5)
-    sk_nav.move_to_object("table")
-
-
-def drive_example(robot, world):
-    robot.update_velocity([0.4, 0.0, 0.0], 0.17)
-    world.step_seconds(10)
-    robot.stop_driving()
-
-
-def predicate_example(scene, robot):
-    preds = Predicates(scene, robot)
-    print("Cube on table: " + str(preds.on("table", "cube1")))
-    print("Cube on box 1: " + str(preds.on("container1", "cube1")))
-
-
-def main():
-    # Command line arguments
-    args = run_util.parse_arguments()
-
-    # Load existing simulation data if desired
-    savedir = os.path.join(BASEDIR, "data", "sim")
-    objects, robot_mdl = run_util.restore_pybullet_sim(savedir, args)
-
-    # Load config file
-    cfg = ConfigYaml(os.path.join(BASEDIR, "config", "main.yaml"))
-
-    # Create world
-    robot, scene = run_util.setup_pybullet_world(
-        ScenePlanning1, BASEDIR, savedir, objects, args, cfg, robot_mdl
-    )
-
-    # Set up skills
-    sk_grasp = SkillGrasping(scene, robot, cfg)
-    sk_place = SkillPlacing(scene, robot)
-    sk_nav = SkillNavigate(scene, robot)
-    sk_move = SkillMove(scene, robot, 0.02, robot._world.T_s)
-
-    # ---------- Run examples -----------
-
-    robot._world.step_seconds(5)
-
-    # drawer_example(sk_grasp, sk_nav, robot, robot._world)
-
-    # drawer_example_auto(sk_grasp, sk_nav, sk_move, robot, scene)
-
-    # grasp_example(sk_grasp, sk_nav, robot, scene, sk_place, object_name="cube1")
-    # grasp_example(sk_grasp, sk_nav, robot, scene, sk_place, object_name="lid1")
-    # grasp_example(sk_grasp, sk_nav, robot, scene, sk_place, object_name="lego")
-    grasp_example(sk_grasp, sk_nav, robot, scene, sk_place, object_name="duck")
-
-    # drive_example(robot, world)
-
-    # predicate_example(scene, robot)
-
-    # navigate_with_cube(sk_nav, sk_grasp)
-
-    # navigation_example(sk_nav, world)
-
-    # -----------------------------------
-
-    robot._world.step_seconds(2)
+    def loop(self):
+        if self.running:
+            self.world.step_one()
 
 
 if __name__ == "__main__":
-    main()
+    # Command line arguments
+    args = run_util.parse_arguments()
+
+    rospy.init_node("hlp_simulator")
+
+    app = SimServer()
+
+    rate = rospy.Rate(240)
+    while not rospy.is_shutdown():
+        app.loop()
+        rate.sleep()
