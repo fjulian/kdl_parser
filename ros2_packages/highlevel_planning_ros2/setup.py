00b2a7a948d7ea21e3e052c126b9f846da2cc053
Julian Förster <julian.foerster@mavt.ethz.ch>
2021-02-22 16:57:58 +0100

    Split for different ROS packages

---
+++ ros2_packages/highlevel_planning_ros2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+try:
+    import ament_index_python
+except ModuleNotFoundError:
+    print("Not a ROS 2 environment. Skipping this one.")
+    exit()
+
+package_name = 'highlevel_planning_ros2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=["highlevel_planning"],
+    package_dir={'': "../../highlevel_planning/src"},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='fjulian',
+    maintainer_email='julian.foerster@mavt.ethz.ch',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
